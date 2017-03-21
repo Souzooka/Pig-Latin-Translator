c2a53d48513f43605eb61bfd3df4547786676c93
Dakota <bazookaharwell@gmail.com>
2017-03-21 09:08:31 -1000

    regex for words starting with vowels

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+function toPigLatin(str) {
+
+  if (str.search(/[aeiou]/gi) === 0 ) {
+    console.log("test");
+  }
+
+
+
+}
+
+function fromPigLatin(str) {
+
+}
+
+toPigLatin("eat");
+toPigLatin("hello");
